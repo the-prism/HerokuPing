9a5c7d6df04c106e115f1fee733d55e7b79a588f
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-29 13:40:23 -0500

    Init small project

---
+++ HerokuPing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+
+namespace HerokuPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heroku Ping");
+            HerokuPing(@"http://verdant-project.herokuapp.com");
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        static void HerokuPing(string address)
+        {
+            Console.WriteLine("Ping adress : {0}, time {1}", address, DateTime.Now.ToString());
+        }
+    }
+}
