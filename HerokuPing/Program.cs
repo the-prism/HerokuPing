0a8142a665cabe59ebcf7874ea0a68e4f989a920
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-29 16:53:49 -0500

    Run every 20 min a http request

--- HerokuPing/Program.cs
+++ HerokuPing/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
-using System.Web;
+using System.Net.Http;
+using System.Timers;
 
 namespace HerokuPing
 {
@@ -13,13 +13,32 @@ namespace HerokuPing
         static void Main(string[] args)
         {
             Console.WriteLine("Heroku Ping");
-            HerokuPing(@"http://verdant-project.herokuapp.com");
+
+            Console.CancelKeyPress += delegate {
+                Environment.Exit(0);
+            };
+
+            TimerEvent();
             System.Threading.Thread.Sleep(2000);
+        }
+
+        private static void TimerEvent()
+        {
+            while(true)
+            {
+                HerokuPing(@"http://verdant-project.herokuapp.com");
+                System.Threading.Thread.Sleep(1000 * 60 * 20);
+            }
         }
 
         static void HerokuPing(string address)
         {
             Console.WriteLine("Ping adress : {0}, time {1}", address, DateTime.Now.ToString());
+            using (HttpClient client = new HttpClient())
+            {
+                var result = client.GetAsync(address).Result;
+                Console.WriteLine(result.StatusCode);
+            }
         }
     }
 }
